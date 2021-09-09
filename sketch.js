d8f79e2025805a791ef2a8dc320c89f96ab86cca
Gunashree S <gunashrees341@gmail.com>
2021-09-09 17:40:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,31 +6,18 @@ var gunImg,bubbleImg, bulletImg, blastImg, backBoardImg;
 var redBubbleGroup, redBubbleGroup, bulletGroup;
 
 
-var life =3;
-var score=0;
-var gameState=1
+
 
 function preload(){
-  gunImg = loadImage("gun1.png")
-  blastImg = loadImage("blast.png")
-  bulletImg = loadImage("bullet1.png")
-  blueBubbleImg = loadImage("waterBubble.png")
-  redBubbleImg = loadImage("redbubble.png")
-  backBoardImg= loadImage("back.jpg")
+ 
+  
 }
 function setup() {
   createCanvas(800, 800);
 
-  backBoard= createSprite(50, width/2, 100,height);
-  backBoard.addImage(backBoardImg)
   
-  gun= createSprite(100, height/2, 50,50);
-  gun.addImage(gunImg)
-  gun.scale=0.2
   
-  bulletGroup = createGroup();   
-  blueBubbleGroup = createGroup();   
-  redBubbleGroup = createGroup();   
+  
   
   heading= createElement("h1");
   scoreboard= createElement("h1");
